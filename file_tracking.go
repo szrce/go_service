d43a885aa94ba6667bc5e98b7d902b686828ef55
Sezer Huseyin <sezerceadres@gmail.com>
2022-02-15 02:08:53 +0300

    Update file_tracking.go
    i adding syslog message support

--- file_tracking.go
+++ file_tracking.go
@@ -1,32 +1,46 @@
 /*
- * created @szrce simple tracking file like a  service
+ * created @szrce simple tracking file
  * 
 */
 
 package main
 
-import "fmt"
-import "os"
-import "time"
+import(
+	"log"
+    	"log/syslog"
+    	"fmt"
+    	"os"
+    	"time"
+)
 
 func main(){
+	//fmt.Println("service started...\n")
+	syslogger, err := syslog.New(syslog.LOG_DEBUG, " @szrce service is started")
 
-	fmt.Println("service started...\n")
+	if err != nil {
+		 fmt.Println(err); 
+		 return
+	}
+
 	for{
 
+		log.SetOutput(syslogger)
+
 		info, err := os.Stat("/root/test.txt")
+
 		if err != nil {
 			 fmt.Println(err); 
 		}
 		x := info.Size()
 
 		if x > 50  {
-			fmt.Printf("%d %s\n",x,"size limit reach")
-			
+			log.Println("size limit reach:",x)
+			//fmt.Printf("%d %s\n",x,"size limit reach")
 			break
 		}
 
-		fmt.Printf("%s %d\n","file size:",x,)
+		log.Println("file size:",x)
+		//fmt.Printf("%s %d\n","file size:",x)
 		time.Sleep(5 * time.Second)
 	}
 }
